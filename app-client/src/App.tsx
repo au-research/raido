781bcf1163498a710780adce339b3aa6d2a8f98b
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2022-08-23 16:11:44 +1000

    fix logo

--- app-client/src/App.tsx
+++ app-client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { RaidoLogoSvg } from "./RaidoLogoSvg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <RaidoLogoSvg />
         <p>
           Raido app-client
         </p>
