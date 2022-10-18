a6937761bbcc6439c45f6a3cdc838383371891bb
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2022-10-18 14:07:39 +1000

    handle non-escaped raid urls

--- api-svc/spring/src/main/java/raido/apisvc/endpoint/raidv2/PublicExperimental.java
+++ api-svc/spring/src/main/java/raido/apisvc/endpoint/raidv2/PublicExperimental.java
@@ -1,13 +1,18 @@
 package raido.apisvc.endpoint.raidv2;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.jooq.DSLContext;
 import org.springframework.context.annotation.Scope;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import raido.apisvc.service.RaidService;
 import raido.apisvc.spring.StartupListener;
 import raido.apisvc.spring.bean.AppInfoBean;
 import raido.apisvc.util.Log;
+import raido.idl.raidv1.model.RaidPublicModel;
 import raido.idl.raidv2.api.PublicExperimentalApi;
 import raido.idl.raidv2.model.PublicReadRaidResponseV1;
 import raido.idl.raidv2.model.PublicServicePoint;
@@ -16,15 +21,26 @@ import raido.idl.raidv2.model.VersionResult;
 import java.util.List;
 
 import static java.time.ZoneOffset.UTC;
+import static java.util.List.of;
+import static org.eclipse.jetty.http.HttpStatus.BAD_REQUEST_400;
 import static org.springframework.context.annotation.ScopedProxyMode.TARGET_CLASS;
+import static raido.apisvc.spring.config.RaidWebSecurityConfig.RAID_V1_API;
+import static raido.apisvc.spring.config.RaidWebSecurityConfig.RAID_V2_API;
+import static raido.apisvc.spring.security.ApiSafeException.apiSafe;
 import static raido.apisvc.util.DateUtil.local2Offset;
+import static raido.apisvc.util.ExceptionUtil.iae;
 import static raido.apisvc.util.Log.to;
+import static raido.apisvc.util.RestUtil.urlDecode;
 import static raido.db.jooq.api_svc.tables.ServicePoint.SERVICE_POINT;
 
 @Scope(proxyMode = TARGET_CLASS)
 @RestController
 @Transactional
 public class PublicExperimental implements PublicExperimentalApi {
+  public static final String HANDLE_URL_PREFIX = "/public/raid/v1";
+  public static final String HANDLE_CATCHALL_PREFIX =
+    RAID_V2_API + HANDLE_URL_PREFIX + "/";
+  public static final String HANDLE_SEPERATOR = "/";
   private static final Log log = to(PublicExperimental.class);
   
   private DSLContext db;
@@ -85,5 +101,40 @@ public class PublicExperimental implements PublicExperimentalApi {
       metadata(data.raid().getMetadata().data());
   }
 
+  /**
+   This method catches all prefixes with path prefix `/v2/raid` and attempts
+   to parse the parameter manually, so that we can receive handles that are
+   just formatted with simple slashes.
+   The openapi spec is defined with the "{raidId}' path param because it makes
+   it more clear to the caller what the url is expected to look like.
+   <p>
+   IMPROVE: should write some detailed/edge-case unit tests for this
+   path parsing logic.
+   */
+  @RequestMapping(
+    method = RequestMethod.GET,
+    value = HANDLE_CATCHALL_PREFIX + "**",
+    produces = {"application/json"}
+  )
+  public PublicReadRaidResponseV1 handleRaidCatchAll(
+    HttpServletRequest req
+  ) {
+    String path = urlDecode(req.getServletPath().trim());
+    log.with("path", req.getServletPath()).
+      with("decodedPath", path).
+      with("params", req.getParameterMap()).
+      info("handleRaidCatchAll() called");
 
+    if( !path.startsWith(HANDLE_CATCHALL_PREFIX) ){
+      throw iae("unexpected path: %s", path);
+    }
+
+    String handle = path.substring(HANDLE_CATCHALL_PREFIX.length());
+    if( !handle.contains(HANDLE_SEPERATOR) ){
+      throw apiSafe("handle did not contain a slash character",
+        BAD_REQUEST_400, of(handle));
+    }
+
+    return publicReadRaid(handle);
+  }
 }
