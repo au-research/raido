281bb4b161f65d2257668bfbd70c1c7767783243
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2022-11-18 09:36:07 +1000

    clean up unused code

--- app-client/src/Component/RaidoLink.tsx
+++ app-client/src/Component/RaidoLink.tsx
@@ -1,5 +1,4 @@
-import React, {ReactNode} from "react";
-import { useLocationPathname } from "Util/Hook/LocationPathname";
+import React, { ReactNode } from "react";
 import { Link } from "@mui/material";
 import { useNavigation } from "Design/NavigationProvider";
 
@@ -7,12 +6,9 @@ export function RaidoLink({href, children}: {
   href: string,
   children?: ReactNode
 }){
-  //const loc = useLocationPathname();
   const nav = useNavigation();
   return <Link href={href}
-    onClick={event=>{
-      //event.preventDefault();
-      //loc.pushState(href);
+    onClick={event => {
       nav.navigateTo(href, event);
     }}
   >{children}</Link>
