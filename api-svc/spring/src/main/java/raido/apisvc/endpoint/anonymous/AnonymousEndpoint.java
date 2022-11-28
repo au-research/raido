d87a713e41e407533e36d6894b123de2d764a5ae
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2022-11-28 11:33:24 +1000

    doc

--- api-svc/spring/src/main/java/raido/apisvc/endpoint/anonymous/AnonymousEndpoint.java
+++ api-svc/spring/src/main/java/raido/apisvc/endpoint/anonymous/AnonymousEndpoint.java
@@ -12,6 +12,11 @@ import static raido.apisvc.spring.config.RaidWebSecurityConfig.PUBLIC;
 @RestController
 public class AnonymousEndpoint {
 
+  /** Be careful with changes to this, it is used by the Auto-scaling group 
+  health-check.
+  For example, do not implement a DB connection check here - that would just
+  result in the ASG cycling EC2 instances constantly, which will do nothing 
+  to resolve the DB issue. */ 
   @GetMapping("/status")
   public Map<String, String> warmUp(){
     return Map.of("status","UP");
