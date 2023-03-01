8156892db6fc81ce8db7801d14ae9c6370d84de8
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2023-03-01 16:21:37 +1000

    force landing page to https to fix int tests

--- api-svc/spring/src/main/java/raido/apisvc/spring/config/environment/EnvironmentProps.java
+++ api-svc/spring/src/main/java/raido/apisvc/spring/config/environment/EnvironmentProps.java
@@ -23,8 +23,21 @@ public class EnvironmentProps {
   @Value("${EnvironmentConfig.rootPathRedirect:https://www.raid.org.au}")
   public String rootPathRedirect;
   
+  /* This has to be http for local testing to work in a real browser, but
+  unexpectedly, setting this to `http` causes int tests to break.  
+  
+  But this needs to be http if you want it to work in your local browser.
+  For the moment, set this in your api-svc-env.properties, but I need to work
+  out how to override for just the int tests (or tell RestTemplate to not 
+  follow redirects, but that's tricky).
+  
+  There's an issue with following redirects when they use different protocols.
+  So it turns out having this set to "https" makes the int tests work because 
+  then the RestTemplate won't follow the redirect:  
+  https://stackoverflow.com/a/1884427/924597  
+  */
   @Value("${EnvironmentConfig.raidoLandingPage:" +
-    "http://localhost:7080/handle" +
+    "https://localhost:7080/handle" +
     "}")
   public String raidoLandingPage;
 
