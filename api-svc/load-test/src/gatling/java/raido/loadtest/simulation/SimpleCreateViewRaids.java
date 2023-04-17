7cf025f52dab38b5d6b90e06958666827afb1c85
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2023-04-17 11:44:06 +1000

    document load-test simple injection profile

--- api-svc/load-test/src/gatling/java/raido/loadtest/simulation/SimpleCreateViewRaids.java
+++ api-svc/load-test/src/gatling/java/raido/loadtest/simulation/SimpleCreateViewRaids.java
@@ -34,6 +34,19 @@ public class SimpleCreateViewRaids extends Simulation {
           injectOpen(
             rampUsers(simConfig.userCount).
               during(simConfig.rampUpSeconds),
+            /* "X users added *per-second* for Y seconds" 
+            Example: Set to 1, and steadyState at 20 seconds, that's 20 raids 
+            minted, 1 per second. 
+            After each raid is minted (which itself takes 4 seconds), then all 
+            the think time and other queries execute in parallel for each user 
+            after that.
+            So you actually end up with at least 3 raids being minted 
+            simultaneously, offset by one second each.
+            Overall, given minting time and think time, you end up with about
+            14 simultaneous "virtual users" "using" the system.
+            A total of 85 Requests Per Second, an avg 2.5 RPS, with a peak at 
+            about 4 RPS (because of initial rampUp phase).  
+            */
             constantUsersPerSec(simConfig.userCount).
               during(simConfig.steadyStateSeconds)
           )
