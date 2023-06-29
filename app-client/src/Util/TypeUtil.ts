50ad57093ccee8c502faa0810f6a5ecb70f42212
Shorn <107518639+ardc-shorn@users.noreply.github.com>
2023-06-29 16:08:41 +1000

    doc

--- app-client/src/Util/TypeUtil.ts
+++ app-client/src/Util/TypeUtil.ts
@@ -25,14 +25,20 @@ export function assert(input: unknown, message?: string)
   }  
 }
 
-/*
- https://stackoverflow.com/a/69328045/924597
- Is there a builtin for this?
-*/
-export type WithRequired<T, K extends keyof T> = T & { 
-  [P in K]-?: T[P] 
+/**
+ * `WithRequired<T, 'propName'>` constructs a type that is the same 
+ * shape as T, but with 'propName' defined as non-optional.
+ * https://stackoverflow.com/a/69328045/924597
+ * Is there a builtin for this?
+ */
+export type WithRequired<T, K extends keyof T> = T & {
+  [P in K]-?: T[P]
 }
 
+/**
+ * `WithStringKey<T>` constructs a type that represents the set of keys in T 
+ * for which the corresponding value is of type string.
+ */
 export type WithStringKey<T> = {
   [K in keyof T]: T[K] extends string ? K : never;
 }[keyof T];
