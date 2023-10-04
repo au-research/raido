2f507c73b44e5262d669537f091addc610e2f925
Rob Leney <rob.leney@ardc.edu.au>
2023-10-04 17:08:20 +1100

    Add CORS configuration for swagger ui endpoint

---
+++ api-svc/raid-api/src/main/java/au/org/raid/api/spring/config/CorsConfiguration.java
@@ -0,0 +1,14 @@
+package au.org.raid.api.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/swagger-ui/index.html");
+    }
+}
