6b1f783ac06c6b668670aab932d638adac088f2d
Rob Leney <rob.leney@ardc.edu.au>
2023-10-05 12:30:47 +1100

    Add OpenApiDefinition annotation to fix problem with swagger ui making http (not https) calls in deployed environments

--- api-svc/raid-api/src/main/java/au/org/raid/api/Api.java
+++ api-svc/raid-api/src/main/java/au/org/raid/api/Api.java
@@ -1,8 +1,10 @@
 package au.org.raid.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Api {
     public static void main(String[] args) {
