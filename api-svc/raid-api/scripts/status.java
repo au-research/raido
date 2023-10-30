d166a0c4f52713f9e84abd6b6a6ad08d914e50b3
Rob Leney <rob.leney@ardc.edu.au>
2023-10-31 10:58:04 +1100

    RAID-54: Add small status script for healthcheck in container as curl is unavailable

---
+++ api-svc/raid-api/scripts/status.java
@@ -0,0 +1,18 @@
+package scripts;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class status {
+    public static void main(String[] args) throws Exception {
+        assert args[0] != null;
+
+        URL url = new URL(args[0]);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+
+        assert responseCode == 200;
+    }
+}
