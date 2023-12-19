5583cef907406be48c710a11797d7c646444db64
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2023-12-20 06:18:13 +1030

    Add json payload test

--- api-svc/raid-api/src/test/java/au/org/raid/api/factory/datacite/DatacitePayloadFactoryTest.java
+++ api-svc/raid-api/src/test/java/au/org/raid/api/factory/datacite/DatacitePayloadFactoryTest.java
@@ -1,21 +1,15 @@
 package au.org.raid.api.factory.datacite;
 
-import au.org.raid.api.model.datacite.DataciteAttributesDto;
 import au.org.raid.api.model.datacite.DataciteDto;
-import au.org.raid.api.model.datacite.DataciteTitle;
 import au.org.raid.idl.raidv2.model.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DatacitePayloadFactoryTest {
 
@@ -28,6 +22,7 @@ public class DatacitePayloadFactoryTest {
     DataciteRelatedIdentifierFactory dataciteRelatedIdentifierFactor = new DataciteRelatedIdentifierFactory();
     DataciteRightFactory dataciteRightFactory = new DataciteRightFactory();
     DataciteTypesFactory dataciteTypesFactory = new DataciteTypesFactory();
+    DataciteAlternateIdentifierFactory dataciteAlternateIdentifierFactory = new DataciteAlternateIdentifierFactory();
     private final DataciteAttributesDtoFactory dataciteAttributesDtoFactory = new DataciteAttributesDtoFactory(
             dataciteTitleFactory,
             dataciteCreatorFactory,
@@ -36,7 +31,8 @@ public class DatacitePayloadFactoryTest {
             dataciteDescriptionFactory,
             dataciteRelatedIdentifierFactor,
             dataciteRightFactory,
-            dataciteTypesFactory
+            dataciteTypesFactory,
+            dataciteAlternateIdentifierFactory
     );
     private final DatacitePayloadFactory datacitePayloadFactory = new DatacitePayloadFactory(dataciteDtoFactory, dataciteAttributesDtoFactory);
 
@@ -58,8 +54,6 @@ public class DatacitePayloadFactoryTest {
         access.setAccessStatement(accessStatement);
 
         raidUpdateRequest.setAccess(access);
-
-
 
         Title title1 = new Title();
         title1.setText("Title 1");
@@ -127,12 +121,83 @@ public class DatacitePayloadFactoryTest {
 
         raidUpdateRequest.setRelatedRaid(List.of(relatedRaid1));
 
+        AlternateIdentifier alternateIdentifier1 = new AlternateIdentifier();
+        alternateIdentifier1.setId("https://doi.org/10.5281/zenodo.1234");
+        alternateIdentifier1.setType("RAiD");
+
+        raidUpdateRequest.setAlternateIdentifier(List.of(alternateIdentifier1));
+
         DataciteDto dataciteDto =  datacitePayloadFactory.payloadForUpdate(raidUpdateRequest, "10.82841/abc123");
 
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(dataciteDto);
+        String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(dataciteDto);
 
-        assertThat(1+1, is(2));
+        String expected = "{\n" +
+                "  \"schemaVersion\" : \"http://datacite.org/schema/kernel-4\",\n" +
+                "  \"type\" : \"dois\",\n" +
+                "  \"attributes\" : {\n" +
+                "    \"prefix\" : \"10.82841\",\n" +
+                "    \"doi\" : \"10.82841/abc123\",\n" +
+                "    \"publisher\" : \"ARDC\",\n" +
+                "    \"publicationYear\" : \"2023\",\n" +
+                "    \"creators\" : [ {\n" +
+                "      \"dataciteCreator\" : \"https://ror.org/038sjwq14\",\n" +
+                "      \"name\" : \"https://ror.org/038sjwq14\"\n" +
+                "    } ],\n" +
+                "    \"contributors\" : [ {\n" +
+                "      \"name\" : \"Name for https://orcid.org/0009-0000-9306-3120\",\n" +
+                "      \"contributorType\" : \"Researcher\"\n" +
+                "    }, {\n" +
+                "      \"name\" : \"Name for https://ror.org/03yrm5c26\",\n" +
+                "      \"contributorType\" : \"ResearchGroup\"\n" +
+                "    } ],\n" +
+                "    \"descriptions\" : [ {\n" +
+                "      \"description\" : \"Description 1\",\n" +
+                "      \"descriptionType\" : \"Abstract\"\n" +
+                "    }, {\n" +
+                "      \"description\" : \"Description 2\",\n" +
+                "      \"descriptionType\" : \"Abstract\"\n" +
+                "    } ],\n" +
+                "    \"relatedIdentifiers\" : [ {\n" +
+                "      \"relatedIdentifier\" : \"https://doi.org/10.5281/zenodo.1234\",\n" +
+                "      \"relatedIdentifierType\" : \"HasPart\"\n" +
+                "    }, {\n" +
+                "      \"relatedIdentifier\" : \"https://example.com/alternate-url-1\",\n" +
+                "      \"relatedIdentifierType\" : \"IsSourceOf\"\n" +
+                "    }, {\n" +
+                "      \"relatedIdentifier\" : \"https://raid.org.au/raid/1234\",\n" +
+                "      \"relatedIdentifierType\" : \"HasPart\"\n" +
+                "    } ],\n" +
+                "    \"rightsList\" : [ {\n" +
+                "      \"rights\" : \"https://creativecommons.org/licenses/by/4.0\",\n" +
+                "      \"rightsUri\" : \"https://creativecommons.org/licenses/by/4.0\"\n" +
+                "    } ],\n" +
+                "    \"titles\" : [ {\n" +
+                "      \"title\" : \"Title 1 (11-2021 through 2021-12-31)\"\n" +
+                "    }, {\n" +
+                "      \"title\" : \"Title 2 (tba through tba)\",\n" +
+                "      \"titleType\" : \"AlternativeTitle\"\n" +
+                "    } ],\n" +
+                "    \"dates\" : [ {\n" +
+                "      \"date\" : \"2021-01-01\",\n" +
+                "      \"dateType\" : \"Available\"\n" +
+                "    } ],\n" +
+                "    \"types\" : {\n" +
+                "      \"resourceType\" : \"RAiD Project\",\n" +
+                "      \"resourceTypeGeneral\" : \"Other\"\n" +
+                "    },\n" +
+                "    \"alternateIdentifiers\" : [ {\n" +
+                "      \"alternateIdentifier\" : \"https://doi.org/10.5281/zenodo.1234\",\n" +
+                "      \"alternateIdentifierType\" : \"RAiD\"\n" +
+                "    } ]\n" +
+                "  }\n" +
+                "}";
+
+        try {
+            JSONAssert.assertEquals(expected, json, false);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
