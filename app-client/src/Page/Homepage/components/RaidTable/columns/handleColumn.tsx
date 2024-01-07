a181a0cd9f4cdbbe92f475b99263947a644f4dbc
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2024-01-08 09:20:12 +1030

    Remove unused CopyButton from handleColumn

--- app-client/src/Page/Homepage/components/RaidTable/columns/handleColumn.tsx
+++ app-client/src/Page/Homepage/components/RaidTable/columns/handleColumn.tsx
@@ -3,8 +3,6 @@ import {GridColDef} from "@mui/x-data-grid";
 import {RaidoLink} from "../../../../../Component/RaidoLink";
 import {TextSpan} from "../../../../../Component/TextSpan";
 import {Stack} from "@mui/material";
-import CopyButton from "Component/CopyButton";
-
 
 export const handleColumn: GridColDef = {
     field: "identifier",
@@ -19,9 +17,7 @@ export const handleColumn: GridColDef = {
                 <RaidoLink href={`/show-raid/${prefix}/${suffix}`}>
                     <TextSpan>{suffix}</TextSpan>
                 </RaidoLink>
-                <CopyButton textToCopy={suffix}/>
             </Stack>
         );
     },
 }
-
