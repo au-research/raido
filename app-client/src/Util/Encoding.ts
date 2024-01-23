485e8083ec90c10f13a31218545a0a182fc53cdf
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2024-01-24 09:33:00 +1030

    Remove unused utils and helper files

--- app-client/src/Util/Encoding.ts
+++
@@ -1,9 +0,0 @@
-
-export function encodeBase64(plainText: string, encodeUri = false): string{
-  let base64Encoded = btoa(plainText);
-  if( !encodeUri ){
-    return base64Encoded;
-  }
-  
-  return encodeURIComponent(base64Encoded);
-}
