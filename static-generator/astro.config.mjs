f604d52d2184aa10e6dc97dd6f9f90c095f5a270
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2024-05-29 17:14:14 +1000

    replace static-generator content with new version

--- static-generator/astro.config.mjs
+++ static-generator/astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [tailwind()],
+  routes: [
+    {
+      pattern: "/raids/:prefix/:suffix",
+      component: "src/raids/[prefix]/[suffix].astro",
+    },
+  ],
 });
