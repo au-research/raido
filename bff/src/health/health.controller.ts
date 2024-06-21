fc893c667ef7addcc136820b0d9b5de57b998409
Rob Leney <rob.leney@ardc.edu.au>
2024-06-21 14:16:18 +1000

    Add JWT token validation

--- bff/src/health/health.controller.ts
+++ bff/src/health/health.controller.ts
@@ -1,10 +1,12 @@
 import {Controller, Get} from '@nestjs/common';
 import {HealthCheck} from "@nestjs/terminus";
+import {Unprotected} from "nest-keycloak-connect";
 
 @Controller('health')
 export class HealthController {
     @Get()
     @HealthCheck()
+    @Unprotected()
     check() {
         return {
             status: 'ok'
--- bff/src/main.ts
+++ bff/src/main.ts
@@ -1,5 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
