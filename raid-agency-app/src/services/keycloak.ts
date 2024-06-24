fa89c0281eb36ef475a3f09e1ae00d6094d35342
weidenhaus-ardc <steffen.weidenhaus@ardc.edu.au>
2024-06-25 06:34:46 +1000

    remove unused keycloak function

--- raid-agency-app/src/services/keycloak.ts
+++ raid-agency-app/src/services/keycloak.ts
@@ -1,12 +1,12 @@
 const kcUrl = import.meta.env.VITE_KEYCLOAK_URL as string;
 const kcRealm = import.meta.env.VITE_KEYCLOAK_REALM as string;
 
-export async function fetchKeycloakGroups({
+export async function fetchCurrentUserKeycloakGroups({
   token,
 }: {
   token: string | undefined;
 }) {
-  const requestUrl = `${kcUrl}/realms/${kcRealm}/group/all`;
+  const requestUrl = `${kcUrl}/realms/${kcRealm}/group/user-groups`;
 
   try {
     if (token === undefined) {
@@ -23,35 +23,6 @@ export async function fetchKeycloakGroups({
     return await response.json();
   } catch (error) {
     const errorMessage = "Error: Keycloak groups could not be fetched";
-    console.error(errorMessage);
-    throw new Error(errorMessage);
-  }
-}
-
-export async function joinKeycloakGroup({
-  groupId,
-  token,
-}: {
-  groupId: string;
-  token: string | undefined;
-}) {
-  const requestUrl = `${kcUrl}/realms/${kcRealm}/group/join`;
-  try {
-    if (token === undefined) {
-      throw new Error("Error: Keycloak token not set");
-    }
-
-    await fetch(requestUrl, {
-      method: "PUT",
-      body: JSON.stringify({ groupId }),
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  } catch (error) {
-    const errorMessage = "Error: Keycloak group could not be joined";
     console.error(errorMessage);
     throw new Error(errorMessage);
   }
