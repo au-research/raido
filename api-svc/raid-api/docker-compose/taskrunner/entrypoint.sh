0d211bad82702f9d50ed67b01f9ac039be580bd8
Rob Leney <rob.leney@ardc.edu.au>
2024-08-15 10:41:28 +1000

    Removed TaskRunner from docker-compose and added migration for dev environment. Removed API from docker-compose - run locally instead.

--- api-svc/raid-api/docker-compose/taskrunner/entrypoint.sh
+++ api-svc/raid-api/docker-compose/taskrunner/entrypoint.sh
@@ -2,57 +2,21 @@
 
 set -e
 
-# Wait for the RAiD API to be ready
-until curl -sSf http://raid-api:8080/actuator/health; do
-  echo "Waiting for application..."
+export PGDATABASE=raido
+export PGHOST=raid-api-db
+export PGPORT=5432
+export PGUSER=postgres
+export PGPASSWORD=supersecret
+
+#until pg_isready -d $PGDATABASE -h $PGHOST -p $PGPORT -U $PGUSER -  ; do
+until pg_isready; do
+  echo "Waiting for ${PGHOST}..."
   sleep 5
 done
 
-echo "Running task..."
+echo "Update service points..."
 
-# Request access token
-response=$(curl --location 'http://raid-iam:8080/realms/raid/protocol/openid-connect/token' \
---data-urlencode 'client_id=raid-api' \
---data-urlencode 'username=raid-operator' \
---data-urlencode 'password=password' \
---data-urlencode 'grant_type=password')
-
-# Extract the access token from the response
-access_token=$(echo $response | jq -r .access_token)
-
-# Update Raid AU service point
-curl --location --request PUT 'raid-api:8080/service-point/20000000' \
---header 'Content-Type: application/json' \
---header "Authorization: Bearer $access_token" \
---data-raw '{
-    "id": 20000000,
-    "name": "raido",
-    "identifierOwner": "https://ror.org/038sjwq14",
-    "techEmail": "web.services@ardc.edu.au",
-    "adminEmail": "web.services@ardc.edu.au",
-    "enabled": true,
-    "appWritesEnabled": true,
-    "repositoryId": "datacite-username",
-    "prefix": "10.82841",
-    "password": "datacite-password",
-    "groupId": "169bd3f3-dd42-4ac0-b89a-fb49648e5eff"
-}'
-
-curl --location --request PUT 'raid-api:8080/service-point/20000002' \
---header 'Content-Type: application/json' \
---header "Authorization: Bearer $access_token" \
---data-raw '{
-    "id": 20000002,
-    "name": "RDM@UQ",
-    "identifierOwner": "https://ror.org/00rqy9422",
-    "techEmail": "",
-    "adminEmail": "",
-    "enabled": true,
-    "appWritesEnabled": true,
-    "repositoryId": "datacite-username",
-    "prefix": "10.82841",
-    "password": "datacite-password",
-    "groupId": "ba0b01a6-726f-464f-b501-454a10096826"
-}'
+psql -c "update raido.api_svc.service_point set prefix = '10.82841', password = 'datacite-password', repository_id = 'datacite-username', group_id = 'ba0b01a6-726f-464f-b501-454a10096826' where id = 20000002"
+psql -c "update raido.api_svc.service_point set prefix = '10.82841', password = 'datacite-password', repository_id = 'datacite-username', group_id = '169bd3f3-dd42-4ac0-b89a-fb49648e5eff' where id = 20000000"
 
 echo "Task completed!"
