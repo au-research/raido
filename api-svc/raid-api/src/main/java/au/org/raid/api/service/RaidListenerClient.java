e1c35cc3165e69c8f647f79bac31804895a9d494
Rob Leney <rob.leney@ardc.edu.au>
2024-09-04 16:06:36 +1000

    RAID-144: Add logging to RaidListenerClient

--- api-svc/raid-api/src/main/java/au/org/raid/api/service/RaidListenerClient.java
+++ api-svc/raid-api/src/main/java/au/org/raid/api/service/RaidListenerClient.java
@@ -4,10 +4,13 @@ import au.org.raid.api.config.properties.RaidListenerProperties;
 import au.org.raid.api.dto.RaidListenerMessage;
 import au.org.raid.api.factory.HttpEntityFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RaidListenerClient {
@@ -17,6 +20,8 @@ public class RaidListenerClient {
 
     public void post(final RaidListenerMessage message) {
         final var httpEntity = httpEntityFactory.create(message);
-        restTemplate.exchange(properties.getUri(), HttpMethod.POST, httpEntity, Void.class);
+
+        final var response = restTemplate.exchange(properties.getUri(), HttpMethod.POST, httpEntity, Void.class);
+        log.debug("Response from Raid Listener: {} {}", response.getStatusCode(), response.getBody());
     }
 }
